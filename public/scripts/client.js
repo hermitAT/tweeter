7ea1fa0b62d1ddae988041ea5f39b18e19c20ca1
Adam Thorne <ahthornev2@gmail.com>
2020-11-18 22:44:08 +0000

    initial implementation of dynamic tweets
    Updated CSS and createTweetElement templating, and set up renderTweets function to loop through a given array of tweet objects, creating the markup for each one and appending it to the container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,44 +4,87 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const differenceInDays = function(date) {
+const differenceInDays = function(created_at) {
 
-  const dateToday = new Date();
-  // ^^ create variable for today's date;
-
-  const timeDifference = dateToday.getTime() - date.getTime();
-  // ^^ find difference in milliseconds between the two dates
-
-  const daysDifference =  timeDifference / (1000 * 3600 * 24);
-  // ^^ calculate # of days between two days, accounting for conversion to seconds, minutes, and hours
-
-  return daysDifference + " days ago.";
+  const createdDate = new Date(created_at);
+  // ^^ create Date object to represent the created_at milliseconds
+  const createdDays = createdDate.getDay();
+  // ^^ get number of days from created date object;
+  return createdDays + " days ago";
 };
+// ^^ return to this function ...
 
-const createTweetElement = function(tweet) {
+$(document).ready(function() {
 
-  const { user, content, created_at } = tweet;
+  const renderTweets = function(tweets) {
 
-  let $tweet = `
-  <article>
-    <div class="tweet-header">
-      <div>
-        <a>${user.avatars}</a>
-        <a>${user.name}</a>
-      </div>
-      <a class="handle">${user.handle}</a>
-    </div>
-    <data class="tweet-body">${content.text}</data>
-    <footer class="tweet-footer">
-      <a>${differenceInDays(created_at)}d</a>
-      <div class="tweet-buttons">
-        <button type="submit">F</button>
-        <button type="submit">R</button>
-        <button type="submit">L</button>
-      </div>
-    </footer>
-  </article>
-  `;
+    for (const tweet of tweets) {
+      // ^^ loop through array of tweet objects..
+      let $tweetToAppend = createTweetElement(tweet);
+      // ^^ pass each tweet through the createTweetElement function, and then append to container below..
+      $('#tweet-container').append($tweetToAppend);
+    }
+  };
 
-  return $tweet;
-};
+
+  const createTweetElement = function(tweet) {
+    const { user, content, created_at } = tweet;
+    // ^^ destructure object for shorter/more readable template literals
+
+    // using template created previously, add template literals to implement dynamic element creation
+    const $tweet = $(`
+      <article>
+        <div class="user-data">
+          <div class="profile">
+            <img src=${user.avatars}/>
+            <a>${user.name}</a>
+          </div>
+          <a class="handle">${user.handle}</a>
+        </div>
+        <div class="tweet-content">
+          <data>${content.text}</data>
+        </div>
+        <footer>
+          <a>${differenceInDays(created_at)}</a>
+          <div class="tweet-buttons">
+            <button class="tweet" type="submit">F</button>
+            <button class="tweet" type="submit">R</button>
+            <button class="tweet" type="submit">L</button>
+          </div>
+        </footer>
+      </article>
+  `);
+    // ^^ return for button implementation, unsure of date function implementation either
+
+    return $tweet;
+  };
+
+  // test/driver code ~~~~
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  renderTweets(data);
+  
+});
