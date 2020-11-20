1a17c980ad9e885451ea95a58379cf144ea8b8d8
Adam Thorne <ahthornev2@gmail.com>
2020-11-20 06:02:41 +0000

    added compose button functionality
    utilized jQuery, w/ the slideToggle and toggleClass methods along with find focus in order to keep the newTweet area hidden on load, and slide into view on click, as well as move cursor into the textarea.

--- public/scripts/compose.js
+++ public/scripts/compose.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
  
   $('.compose').on('click', function(event) {
+    // ^^ on click on compose button, slideToggle the form into view and toggle the .hidden class on the .new-tweet form. 
+    // use find() and focus() to move cursor to textarea when form slides into view
 
     $('.new-tweet').slideToggle('slow');
     $('.new-tweet').toggleClass('hidden');
